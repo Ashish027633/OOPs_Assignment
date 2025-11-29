2890c8c8762bc27b41fcdf05fe8f35e594b3e011
Ashish027633 <ashishsharma275201@gmail.com>
2025-11-29 10:16:24 +0530

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,25 @@
+//1.1 write a program in c++ enter your detail & print output
+#include<iostream>
+using namespace std;
+int main()
+{
+char name [20];
+char cname [20];
+float fees ;
+int rollno;
+
+cout<<"enter your name=";
+cin>>name;
+cout<<"\nenter your cname=";
+cin>>cname;
+cout<<"\nenter the fees=";
+cin>>fees;
+cout<<"\nenter the rollno=";
+cin>>rollno;
+cout<<"\nname="<<name<<endl;
+cout<<"\ncname="<<cname<<endl;
+cout<<"\nfees="<<fees<<endl;
+cout<<"\nrollno="<<rollno<<endl;
+
+return 0;
+}
---
+++ 02.cpp
@@ -0,0 +1,17 @@
+//1.2 write a program in c++ enter two number &perform arthmatic opertion and print output
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+  cout<<"enter the first number="; 
+  cin>>a;
+  cout<<"enter the first number=";
+  cin>>b; 
+  cout<<"addition="<<a+b<<endl;
+  cout<<"subtraction="<<a-b<<endl;
+  cout<<"multiplication="<<a*b<<endl;
+  cout<<"division="<<a/b<<endl;
+  cout<<"modulus="<<a%b<<endl;
+ return 0;
+}
